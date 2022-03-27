a4427c14365b8ca1b248f1ec38f9bb59e808fd43
Henrywch228 <98314244+Henrywch228@users.noreply.github.com>
2022-03-27 15:44:46 +0800

    Update game.js

--- game/game.js
+++ game/game.js
@@ -1,23 +1,70 @@
-var character = document.getElementById("character");
-var block = document.getElementById("block");
-var counter=0;
-function jump(){
-    if(character.classList == "animate"){return}
-    character.classList.add("animate");
-    setTimeout(function(){
-        character.classList.remove("animate");
-    },300);
+let container = document.querySelector("#container");
+let dino = document.querySelector("#dino");
+let block = document.querySelector("#block");
+let road = document.querySelector("#road");
+let cloud = document.querySelector("#cloud");
+let score = document.querySelector("#score");
+let gameOver = document.querySelector("#gameOver");
+
+//declaring variable for score
+let interval = null;
+let playerScore = 0;
+
+
+//function for score
+let scoreCounter = () => {
+    playerScore++;
+    score.innerHTML = `Score <b>${playerScore}</b>`;
 }
-var checkDead = setInterval(function() {
-    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
-        block.style.animation = "none";
-        alert("Game Over. score: "+Math.floor(counter/100));
-        counter=0;
-        block.style.animation = "block 1s infinite linear";
-    }else{
-        counter++;
-        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+
+
+//start Game
+window.addEventListener("keydown", (start) => {
+    //console.log(start);
+
+    if (start.code == "Space") {
+        gameOver.style.display = "none";
+        block.classList.add("blockActive");
+        road.firstElementChild.style.animation = "roadAnimate 1.5s linear infinite";
+        cloud.firstElementChild.style.animation = "cloudAnimate 50s linear infinite";
+
+        //score function
+        let playerScore = 0;
+        interval = setInterval(scoreCounter, 200);
+    }
+});
+
+
+//jump Your Character
+window.addEventListener("keydown", (e) => {
+    //console.log(e);
+
+    if (e.key == "ArrowUp")
+        if (dino.classList != "dinoActive") {
+            dino.classList.add("dinoActive");
+
+            //remove class after 0.5 seconds
+            setTimeout(() => {
+                dino.classList.remove("dinoActive");
+            }, 500);
+        }
+});
+
+//'Game Over' if 'Character' hit The 'Block' 
+let result = setInterval(() => {
+    let dinoBottom = parseInt(getComputedStyle(dino).getPropertyValue("bottom"));
+    //console.log("dinoBottom" + dinoBottom);
+
+    let blockLeft = parseInt(getComputedStyle(block).getPropertyValue("left"));
+    //console.log("BlockLeft" + blockLeft);
+
+    if (dinoBottom <= 80 && blockLeft >= 10 && blockLeft <= 80) {
+        //console.log("Game Over");
+        gameOver.style.display = "block";
+        block.classList.remove("blockActive");
+        road.firstElementChild.style.animation = "none";
+        cloud.firstElementChild.style.animation = "none";
+        clearInterval(interval);
+        playerScore = 0;
     }
 }, 10);
