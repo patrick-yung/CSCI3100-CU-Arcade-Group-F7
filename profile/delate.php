4a02f0d65bfd37a8b4f67be25cc680c97f260229
Yung Pak Hong Patrick <79505817+patrick-yung@users.noreply.github.com>
2022-03-29 17:47:24 +0800

    Update delate.php

--- profile/delate.php
+++ profile/delate.php
@@ -3,79 +3,60 @@
 ?>
 <?php include_once '../includes/dbh.inc.php'?>
 <?php include_once 'Format/header.php'?>
-<?php include_once '../includes/admins.php'?>
-
 
 <body>
-    <div class=empty2>
-        
-    </div>
-
 <link rel="stylesheet" href = "Format/style.css">
+
+<div class='empty'>
+</div>
 
 <?php
 
 $currentname=$_SESSION["usersID"];
-
-$sql = "SELECT * FROM users where usersID = $currentname" ;
+$sql = "SELECT * FROM users" ;
 
 //
 $result = mysqli_query($conn, $sql);
 $resultcheck = mysqli_num_rows($result);
-
 if($resultcheck>0){
-    $row = mysqli_fetch_assoc($result);
-    $id=$_SESSION["usersID"];
 
-    $sqlImg = "SELECT * FROM profileimg WHERE userid = '$id'";
-    $resultImg = mysqli_query($conn, $sqlImg);
-    $rowImg=mysqli_fetch_assoc($resultImg);
 
-    echo "<div class='card'>";
-        if($rowImg['have']== 2){
-            echo "<h1>HELLO</h1>";
-            echo "<img src='Upload/profile".$id.".png'".mt_rand().">";
-        }else{
-            echo "<img src='Upload/deafult.png'>";
+    while($row = mysqli_fetch_assoc($result)){;
+        $id = $row['usersID'];
+        $sqlImg = "SELECT * FROM profileimg WHERE userid = '$id'";
+        $resultImg = mysqli_query($conn, $sqlImg);
+        while($rowImg = mysqli_fetch_assoc($resultImg)){
+        echo "<table>";
+            echo "<tr>";
+            if($rowImg['have']== 2){
+                echo "<img src='Upload/profile".$id.".png'".mt_rand().">";
+            }else{
+                echo "<img src='Upload/deafult.png'>";
+            }
+
+            //
+            echo "
+                 <tr>".$row['usersID']."</tr>
+                <tr>".$row['usersName']."</tr>
+                <tr>".$row['usersEmail']."</tr>
+                <tr>
+                <tr><a href='delate_function.php?id=".$row['usersID']."' id='btn'>Delete</a></td>  
+                <tr><a href='reset_function.php?id=".$row['usersID']."' id='btn'>Reset</a></td>  
+                </tr>
+            </tr>
+            "; 
+            
+        echo "</table>";
         }
+    }
+}
 
-        //
-        echo "<p>";
-        echo $row['usersName'];
-        echo "</p>";
-        echo "<p>";
-        echo $row['usersEmail'];
-        echo "</p>";
-  }
-
-      if(isset($_SESSION["usersName"])){
-          echo "You are Login In ";
-          echo "<p>HELLO THERE: ". $_SESSION["usersName"] . "</p>";
-          echo "<form action ='upload.php' method='POST' enctype='multipart/form-data'>
-          <input type ='file' name='file'>
-          <button type='submit' name='submit'>Upload Profile Picture</button>
-                  </form>";
-      }else{
-        header("location: Settings.php");
-    }                 
-    
-
+      
+                       
+  
 ?>
-
-<form action="changepassword.php" method="post">
-			<input type="text" autocomplete="off" name="newpassword" placeholder="New password" />
-			<button type="change" name="change" >Change Now</button>
-            </form>
 
 </body>
-<?php
-if($_SESSION["user_level"]==1){
-    echo "<p><a href='delate.php'>Delate Other Users</a> <p>";
-}
-?>
-</div>;
-
 
 <?php include_once 'Format/footer.php'?>
-
 
