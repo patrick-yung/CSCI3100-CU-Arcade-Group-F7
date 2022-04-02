f961b99cc4276b43ed379ec9a7f7bdcf8c3139c7
Yung Pak Hong Patrick <79505817+patrick-yung@users.noreply.github.com>
2022-04-02 21:11:40 +0800

    Create compare.php

---
+++ game/compare.php
@@ -0,0 +1,26 @@
+<?php
+    session_start();
+?>
+<?php include_once '../includes/dbh.inc.php'?>
+
+<?php
+    $score = $_GET["score"];
+    $id=$_SESSION["usersID"];
+    $sqlImg = "SELECT * FROM leaderboard WHERE usersID = '$id'";
+    $resultImg = mysqli_query($conn, $sqlImg);
+    $rowImg=mysqli_fetch_assoc($resultImg);
+
+    if($rowImg["highestscore"] < $score){
+        $sql = "UPDATE leaderboard set highestscore='$score' WHERE usersID= '$id'";
+        $result = mysqli_query($conn, $sql);
+        if ($result) {  
+            echo "Error: ".$score;  
+
+        }else{  
+            echo "Error: ".mysqli_error($conn);  
+       }  
+    }else{
+    header("location: game.php");
+    }
+
+?>
