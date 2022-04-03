6bce80a1ec19d658c7866b4d63e05e70ef1254a3
lleeooaa <68342721+lleeooaa@users.noreply.github.com>
2022-04-03 15:48:24 +0800

    Update leaderboard.php

--- leaderboard/leaderboard.php
+++ leaderboard/leaderboard.php
@@ -1,8 +1,44 @@
 <?php
     session_start();
+
 ?>
 <?php include_once 'Format/header.php'?>
 
+<style>
+    table, th, td {
+        border:1px solid black;
+        border-collapse: collapse;
+    }
+    table.center {
+        margin-left: auto; 
+        margin-right: auto;
+    }
+</style>
+    <body>
+    <table style="width:50%", class="center">
+        <tr>
+            <td>Rank</td>
+            <td>User</td>
+            <td>Score</td>
+        </tr>
+        <?php
+        $con = mysqli_connect("localhost", "root", "", "leaderboard");
+            
 
+        $result = mysqli_query($con, "SELECT highestscore, username FROM leaderboard ORDER BY highestscore DESC");
+        
+        $ranking = 1;
+        if (mysqli_num_rows($result)) {
+            while ($row = mysqli_fetch_array($result)) {
+                echo "<tr><td>{$ranking}</td>
+                <td>{$row['username']}</td>
+                <td>{$row['highestscore']}</td></tr>";
+                $ranking++;  
+            }
+        }
+        ?>
+    </table>
+
+    </body>
 
 <?php include_once 'Format/footer.php'?>
