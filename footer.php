1a3d86f9078dd0ae4c0e1e30e1f4180cf51ba3d2
Henrywch228 <98314244+Henrywch228@users.noreply.github.com>
2022-04-07 13:34:32 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,9 @@
 
-</body>
-</html>
-
-<script src="js/script.js"></script>
+  <a class = "social-icon" href = "https://www.instagram.com/cu_arcade/?utm_medium=copy_link" target = "_blank">
+      <img src = "../instagram.png">
+  </a>
+      
+  </body>
+  </html>
+  
+  <script src="js/script.js"></script>
