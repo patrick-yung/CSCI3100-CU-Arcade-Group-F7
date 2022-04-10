602549dc71c3ce9f1acd84ddd0016743c58c060b
Yung Pak Hong Patrick <79505817+patrick-yung@users.noreply.github.com>
2022-04-10 16:21:48 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
   <footer>
-        <p style="color:white"><p class = "text">Find out more information about us on Instagram!</p>
+        <p style="color:white"><p class = "text" style="color:white">Find out more information about us on Instagram!</p>
         <a href = "https://www.instagram.com/cu_arcade/">
           <img class = "IG" alt = "IG" src = "instagram.png">
         </a>
