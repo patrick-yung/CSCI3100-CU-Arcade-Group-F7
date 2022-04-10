63750e4dbd9f1a4d99cb3f9b7828b956bc7a7393
Henrywch228 <98314244+Henrywch228@users.noreply.github.com>
2022-04-10 16:52:41 +0800

    Add files via upload

---
+++ home page/Format/footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <p style="color:white"><p class = "text">Find out more information about us on Instagram!</p>
+    <a href = "https://www.instagram.com/cu_arcade/">
+      <img class = "IG" alt = "IG" src = "instagram.png">
+    </a>
+</footer>
+  
+</body>
+</html>
