aba0178b001f4d66b62ddd663981376294df5964
Henrywch228 <98314244+Henrywch228@users.noreply.github.com>
2022-04-12 23:15:34 +0800

    Update home.php

--- home_page/home.php
+++ home_page/home.php
@@ -16,33 +16,53 @@
 </head>
 
 <body>
-    
-    <div id = "container">
-        <img src = "Assets/arcade.png" alt = "container">
-    
-        <div id = "logo">
-            <img src = "Assets/CU_Arcade.png" alt = "logo">
-        </div> 
-    
-    
-        <div id = "intro">
-    
-            <h3>About CU Arcade</h3>
-               
-            <p style="color:white"><p class = "text">
-                In this day and age, due to the COVID-19 situation in Hong Kong, university students are hard to meet each other face to face 
-                in the campus. As students seldom have interaction during the online lessons, they hard to make new friends during the online 
-                University life. Besides, many entertainment facilities such as cinema and theme park were forced to closed under the pandemic 
-                situation, which severely limited the leisure activities that students can enjoy.  Therefore, as a group of computer software 
-                developer, we would like to introduce a new entertaining-base web application to university students in CUHK.
-            </p>
-        </div>
-    
-        <a href = "../Loginandregister/Register.php">
+    <div>
+    <div class="home-container">
+      <img
+        src="Assets/arcade.png"
+        alt="image"
+        class="arcade"
+      />
+      <img
+        src="Assets/CU_Arcade.png"
+        alt="image"
+        class="icon"
+      />
+      <span class="home-text">About CU Arcade</span>
+
+      <span class="home-text1">
+        <span class="home-text2">
+            In this day and age, due to the COVID-19 situation in Hong Kong,
+            university students are hard to meet each other face to face
+          </span>
+          <span class="home-text3">
+            in the campus. As students seldom have interaction during the online
+            lessons, they hard to make new friends during the online
+          </span>
+          <span class="home-text4">
+            University life. Besides, many entertainment facilities such as cinema
+            and theme park were forced to closed under the pandemic
+          </span>
+          <span class="home-text5">
+            situation, which severely limited the leisure activities that students
+            can enjoy. Therefore, as a group of computer software
+          </span>
+          <span class="home-text6">
+            developer, we would like to introduce a new entertaining-base web
+            application to university students in CUHK.
+          </span>
+      </span>
+        
+
+      <span class = "login">
+        <a href="../Loginandregister/Register.php" target="_blank" rel="noreferrer noopener" class="home-link button">
             <img div id = "login" alt = "login" src = "Assets/login.png">
         </a>
-    
+      </span>
+
     </div>
+  </div>
+  
 
 </body>
 
