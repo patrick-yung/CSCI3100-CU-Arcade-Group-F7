85550035bda1e59793d482c20f461b60b7eb2003
Yung Pak Hong Patrick <79505817+patrick-yung@users.noreply.github.com>
2022-04-12 22:37:48 +0800

    Update changepassword.php

--- profile/changepassword.php
+++ profile/changepassword.php
@@ -8,6 +8,12 @@ Function: Change current user's password
 
 
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+require '../vendor/autoload.php';
+
+
 session_start();
 include_once '../includes/dbh.inc.php';
 $id = $_SESSION['usersID'];
@@ -19,5 +25,29 @@ if(isset($_POST['change'])){
     $sql = "UPDATE users set usersPwd='$hashed' WHERE usersID= '$id'";
     $result = mysqli_query($conn, $sql);
 }
+$sql = "SELECT * FROM users where usersID = '$id'" ;
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
+
+
+$mail = new PHPMailer(true);
+$mail->SMTPDebug = 0;//SMTP::DEBUG_SERVER;
+
+$mail->isSMTP();
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPAuth = true;
+$mail->Username = 'csci3100tmp@gmail.com';
+$mail->Password = 'csci31002022';
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+$mail->setFrom('csci3100tmp@gmail.com', 'CSCI3100');
+$mail->addAddress($row['usersEmail'], $row['usersID']);
+$mail->isHTML(true);
+$mail->Subject = 'Your Password Has been changed ';
+$mail->Body    = '<p>Your Password Has been changed </p>';
+$mail->send();
+
+
+
 die(" Successfully Change passsword <a href='Settings.php?'>back</a>");
 header("location: upload.php");
