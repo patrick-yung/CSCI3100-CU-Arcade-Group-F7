80fdc7781558f395788d2afd456b7dfcb2eed48e
Yung Pak Hong Patrick <79505817+patrick-yung@users.noreply.github.com>
2022-04-12 22:57:16 +0800

    Create contactpage.php

---
+++ contact/contactpage.php
@@ -0,0 +1,5 @@
+
+<form action="contact.php" method="post">
+			<input type="text" autocomplete="off" name="qwer" placeholder="message" />
+			<button type="send" name="send" >message</button>
+            </form>
