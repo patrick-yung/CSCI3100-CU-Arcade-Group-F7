fbdc8c46656e55e6790fffcadda852321a1b7e2e
Henrywch228 <98314244+Henrywch228@users.noreply.github.com>
2022-04-12 23:25:42 +0800

    Update footer.php

--- home_page/Format/footer.php
+++ home_page/Format/footer.php
@@ -1,4 +1,16 @@
 <footer>
+    <style>
+      p.text{
+        text-align: center;
+      }
+      img.IG{
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 40;
+        height: 40;
+      }
+     </style> 
     <p style="color:white"><p class = "text">Find out more information about us on Instagram!</p>
     <a href = "https://www.instagram.com/cu_arcade/">
       <img class = "IG" alt = "IG" src = "Assets/instagram.png">
