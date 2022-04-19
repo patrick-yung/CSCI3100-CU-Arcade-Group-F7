a8cbb36f0adbdd0aee87fa8945116a7b2ad855ee
Yung Pak Hong Patrick <79505817+patrick-yung@users.noreply.github.com>
2022-04-19 12:22:25 +0800

    Update success.php

--- profile/success.php
+++ profile/success.php
@@ -1,1 +1,6 @@
 
+/*
+Author: Patrick Yung
+Last Update:04/04/2022
+Function: Only to redirct the user after clicking onto change password, doesn't have a important function, just a redirct php to display an success change password message.
+*/
